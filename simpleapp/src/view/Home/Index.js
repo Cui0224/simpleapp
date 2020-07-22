e29cbe8fb7372675693d52b2509cd5b594f7bc08
wangBug <1030953940@qq.com>
2020-07-22 20:58:24 +0800

    11

--- simpleapp/src/view/Home/Index.js
+++ simpleapp/src/view/Home/Index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
-
+import HomeIndexCss from "../../css/wyb/HomeIndex.module.css"
+import Swiper from "../../component/Swiper"
 export default class Index extends Component {
     render() {
         return (
-            <div>
-                index
+            <div className={HomeIndexCss.container}>
+                <div className={HomeIndexCss.swiper}>
+                    <Swiper/>
+                </div>
             </div>
         )
     }
