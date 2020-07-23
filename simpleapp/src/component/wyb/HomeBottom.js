2b5571c09a8f0a24b887198da043181c39d19265
wangBug <1030953940@qq.com>
2020-07-23 20:57:55 +0800

    ww

--- simpleapp/src/component/wyb/HomeBottom.js
+++ simpleapp/src/component/wyb/HomeBottom.js
@@ -9,20 +9,20 @@ import { withRouter } from "react-router-dom";
       let {path}=this.props.match
     console.log(this.props);
     return (
-      <div className={HomeBottomCss.footer}>
-        <NavLink to={path+"/index"}>
+      <div className={HomeBottomCss.footer} >
+        <NavLink to={path+"/index"} activeClassName="active1">
           <span className="iconfont icon-shouye"></span>
           <p>首页</p>
         </NavLink>
-        <NavLink to={path+"/theater"}>
+        <NavLink to={path+"/theater"} activeClassName="active1">
           <span className="iconfont icon-yingyuan"></span>
           <p>剧院</p>
         </NavLink>
-        <NavLink to={path+"/lol"}>
+        <NavLink to={path+"/lol"} activeClassName="active1">
           <span className="iconfont icon-LOL"></span>
           <p>吐了</p>
         </NavLink>
-        <NavLink to={path+"/personal"}>
+        <NavLink to={path+"/personal"} activeClassName="active1">
           <span className="iconfont icon-yixianshi_huaban"></span>
           <p>我的</p>
         </NavLink>
